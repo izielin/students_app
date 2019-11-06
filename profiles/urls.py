599988e3a85eb544d21c412a077848655882e030
izielin <misiopar@wp.pl>
2019-11-06 19:44:38 +0100

    search bar on profile_list side

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/<int:pk>', views.profile, name='profile'),
-    path('profile/list/', views.ProfileListView.as_view(), name='profile_changelist'),
+    path('profile/list/', views.profiles_list, name='profile_changelist'),
     path('profile/add/', views.ProfileCreateView.as_view(), name='profile_add'),
     path('profile/<int:pk>/edit', views.ProfileUpdateView.as_view(), name='profile_change'),
     path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Profile, City
 from .forms import ProfileForm
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-class ProfileListView(ListView):
-    model = Profile
-    context_object_name = 'profiles'
+def profiles_list(request):
+    profile_list = Profile.objects.all()
+    query = request.GET.get('q')
+    if query:
+        profile_list = Profile.objects.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query)
+        ).distinct()
+
+    paginator = Paginator(profile_list, 15)  # 15 profiles per page
+    page = request.GET.get('page')
+
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        profiles = paginator.page(1)
+    except EmptyPage:
+        profiles = paginator.page(paginator.num_pages)
+
+    context = {
+        'profiles': profiles
+    }
+    return render(request, "profiles/profile_list.html", context)
 
 
 class ProfileCreateView(CreateView):
